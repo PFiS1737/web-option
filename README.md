3bbce6b9f441cd8ff33bea5e7a4194196d457f40
PFiS1737 <3165823482@qq.com>
2021-12-31 20:36:48 +0800

    feat: 创建 web-option 项目

---
+++ README.md
@@ -0,0 +1,89 @@
+# web-option
+
+> 一个在 localStorage 中管理页面设置的简单解决方案。
+> A simple solution to manage page settings in localStorage
+
+## 注意 | Attention
+- 由于 localStorage 是同步操作的，所以不要储存过大的数据，否则会造成页面卡顿
+- Demo: [demo.html](https://PFiS1737.github.io/web-option/demo.html)
+
+## 使用 | Use
+
+```html
+<script type="module">
+    import { WebOption } from "./index.js"
+    let wo = new WebOption()
+    wo.addItem("foo", ["foo1", "foo2", "foo3", "foo4","foo5"], (selected, original) => {
+        console.log("foo -> from", original, "to", selected)
+    }).addItem("bar", ["bar1", "bar2", "bar3", "bar4"], (selected, original) => {
+        console.log("bar -> from", original, "to", selected)
+    }).init(initResult => console.log({initResult}))
+    window.wo = wo
+</script>
+```
+
+- Class `WebOption(namespace)`
+    - 生成一个 WebOption 实例
+    - 参数
+        - namespace: `string`  
+          设置在 `localStorage` 中储存的名称
+    - 返回值: `WebOption`
+- `WebOption.init(...handlers)`
+    - 初始化
+    - 参数
+        - handlers: `Function(initResult)`  
+          设置在初始化结束后执行的函数，接受一个参数，是对 `WebOption.getItemValMap()` 的引用
+    - 返回值: `void`
+- `WebOption.addItem(name, values, handler)`
+    - 添加设置
+    - 参数
+        - name: `string`  
+          指定设置名称
+        - values: `Array`  
+          设置所有允许的参数
+        - handler: `Function(selected, original)`  
+          指定在每次成功更改该设置的值时执行的函数，接受两个参数，分别是刚设置的值和原先的值
+    - 返回值: `WebOption`
+- `WebOption.hasItem(name)`
+    - 检测是否有指定名称的设置
+    - 参数
+        - name: `string`  
+          指定设置名称
+    - 返回值: `bealoon`
+- `WebOption.getItem(name)`
+    - 获得对应名称的 OptionItem 实例
+    - 参数
+        - name: `string`  
+          指定设置名称
+    - 返回值: `OptionItem`
+- `WebOption.setItemVal(name, value, handler)`
+    - 设置对应设置的值
+    - 参数
+        - name: `string`  
+          指定设置名称
+        - values: any possible  
+          指定要设置的值，如果该值不在 `addItem` 时设置的 `values` 中，则不会更改，也不报错
+        - handler: `Function(selected, original)`  
+          指定在更改成功时执行的函数，接受两个参数，分别是刚设置的值和原先的值
+    - 返回值: `WebOption`
+- `WebOption.toggleItemVal(name)`
+    - 切换对应设置的值
+    - 参数
+        - name: `string`  
+          指定设置名称
+    - 返回值: `WebOption`
+- `WebOption.getItemVal(name)`
+    - 获得对应设置的值
+    - 参数
+        - name: `string`  
+          指定设置名称
+    - 返回值: anything possible
+- `WebOption.getItemValMap()`
+    - 获得所有设置的值
+    - 返回值: `Object`
+- `WebOption.setStorage()`
+    - 将设置添加到 localStorage
+    - 返回值: `void`
+- `WebOption.getStorage()`
+    - 从 localStorage 读取设置
+    - 返回值: `void`
---
+++ index.js
@@ -0,0 +1,101 @@
+export class WebOption {
+    constructor(namespace = "WebOption") {
+        this._namespace = namespace
+        this.items = {}
+    }
+    init(...handlers) {
+        this.getStorage()
+        this.setStorage()
+        let initResult = this.getItemValMap()
+        handlers.forEach(handler => handler(initResult))
+        this.addItem = undefined
+        this.getStorage = undefined
+        this.init = undefined
+    }
+    addItem(name, values, handler) {
+        this.items[name] = new OptionItem(name, values, handler)
+        return this
+    }
+    hasItem(name) {
+        return this.getItem(name) ? true : false
+    }
+    getItem(name) {
+        return this.items[name]
+    }
+    setItemVal(name, value, handler = () => {}) {
+        let item = this.getItem(name)
+        let result = item?.select(value)
+        if (result) handler(item.selected, item.original)
+        this.setStorage()
+        return this
+    }
+    toggleItemVal(name) {
+        this.getItem(name)?.toggle()
+        this.setStorage()
+        return this
+    }
+    getItemVal(name) {
+        return this.getItem(name)?.selected
+    }
+    getItemValMap() {
+        let result = {}
+        forEachObject(this.items, key => result[key] = this.getItemVal(key))
+        return result
+    }
+    setStorage() {
+        let storage = this.getItemValMap()
+        localStorage.setItem(this._namespace + ":all", JSON.stringify(storage))
+    }
+    getStorage() {
+        let storage = JSON.parse(localStorage.getItem(this._namespace + ":all") ?? "{}")
+        forEachObject(storage, (key, value) => this.setItemVal(key, value))
+    }
+}
+
+class OptionItem {
+    constructor(name, values = [], handler = () => {}) {
+        this.name = name
+        this.values = new Set(values)
+        this.handler = handler
+        this.selected = values[0]
+        this.original = undefined
+    }
+    select(value) {
+        if (this.hasVal(value)) {
+            this.original = this.selected
+            this.selected = value
+            this.handler(this.selected, this.original)
+            return true
+        }
+        else return false
+    }
+    toggle() {
+        let _values = [...this.values]
+        for (let i = 0; i < _values.length; i++) {
+            let item = _values[i]
+            if (this.selected === item) {
+                if (i < _values.length - 1) this.select(_values[i + 1])
+                else this.select(_values[0])
+                break
+            }
+        }
+    }
+    addVal(value) {
+        this.values.add(value)
+        return this
+    }
+    delVal(value) {
+        this.values.delete(value)
+        return this
+    }
+    hasVal(value) {
+        return this.values.has(value)
+    }
+}
+
+function forEachObject(obj, callback, thisArg = this) {
+    Object.keys(obj).forEach((key, i) => {
+        let value = obj[key]
+        callback.call(thisArg, key, value, i, obj)
+    })
+}
